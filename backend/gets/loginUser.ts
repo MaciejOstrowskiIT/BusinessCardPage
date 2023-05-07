50998305f536d79c72281826de2a8b0fb3ac96cd
Maciej Ostrowski <maciej.ostrowski.it@icloud.com>
2023-05-07 21:14:06 +0200

    added hashed password checning in loginUser.ts

--- backend/gets/loginUser.ts
+++ backend/gets/loginUser.ts
@@ -1,24 +1,22 @@
 import fs from "fs";
 import path from "path";
-import {Request, Response} from "express";
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
 
 export const loginUser = (req: Request, res: Response) => {
     const { username, password } = req.body;
-    const data: string = JSON.stringify({ username, password }) + "\n";
-    const users = JSON.parse(data)
-    const directoryPath = './database'
+    const directoryPath = "./database";
 
     let userFound: boolean = false;
 
     fs.readdir(directoryPath, function (err: any, files: any) {
         if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        }
-        else {
+            return console.log("Unable to scan directory: " + err);
+        } else {
             let filePromises = [];
 
             for (let i = 0; i < files.length; i++) {
-                const file = files[i]
+                const file = files[i];
                 const fileName = path.parse(file).name;
                 if (fileName === username) {
                     console.log("Username found");
@@ -30,12 +28,16 @@ export const loginUser = (req: Request, res: Response) => {
                                 if (err) return reject(err);
                                 const obj = JSON.parse(data);
                                 const passwordLoaded = obj.password;
-                                if (passwordLoaded === users.password) {
-                                    console.log("Password correct");
-                                    return resolve("");
-                                } else {
-                                    return reject(new Error("Incorrect password"));
-                                }
+                                bcrypt.compare(password, passwordLoaded, function (err, result) {
+                                    if (err) {
+                                        return reject(err);
+                                    } else if (result) {
+                                        console.log("Password correct");
+                                        return resolve("");
+                                    } else {
+                                        return reject(new Error("Incorrect password"));
+                                    }
+                                });
                             });
                         })
                     );
