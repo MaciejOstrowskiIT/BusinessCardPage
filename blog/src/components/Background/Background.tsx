6fec88fb70e6076da365a9c00a6510b01d7f0b02
Maciej Ostrowski <maciej.ostrowski.it@icloud.com>
2023-05-28 23:39:23 +0200

    added background

---
+++ blog/src/components/Background/Background.tsx
@@ -0,0 +1,12 @@
+import App from "../../App.tsx";
+import './Background.css'
+import React from "react";
+
+const Background: React.FC = () => {
+    return (
+        <div className={"background"}>
+        <App/>
+        </div>
+    )
+}
+export default Background
