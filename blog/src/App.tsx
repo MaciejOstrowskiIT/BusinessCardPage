b9a2bac5e62c33537909a38797d8a11b3571ffdb
Maciej Ostrowski <maciej.ostrowski.it@icloud.com>
2023-05-29 00:14:26 +0200

    changed long lorem into map

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -6,104 +6,20 @@ import CookiePrompt from "./components/footer/CookiePrompt/CookiePrompt.tsx";
 
 function App() {
 
+    const loremQuote = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime" +
+        "obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae" +
+        "mollitia quo repudiandae sapiente ";
+
+    const quoteElements = Array(50).fill(loremQuote).map((quote, index) => (
+        <p key={index}>{quote}</p>
+    ));
+
     return (
         <>
             <div>
                 <Nav/>
                 <img src={reactLogo} width="120" alt=""/>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid delectus iste maxime
-                    obcaecati officia sed ullam voluptas? Accusantium esse itaque odio totam vitae! Aperiam beatae
-                    mollitia quo repudiandae sapiente.</p>
+                {quoteElements}
 
                 <CookiePrompt/>
             </div>
